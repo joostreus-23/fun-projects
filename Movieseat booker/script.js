2ddacd1de9281aaa201b5fd1ccfd1cde7b837067
Joost Reus <twenty-three_design@outlook.com>
2020-07-02 17:20:46 +0200

    html plus javascript

---
+++ Movieseat booker/script.js
@@ -0,0 +1,65 @@
+const filmOpties = document.getElementById("films");
+const stoelenLijst = document.getElementById("stoelenlijst");
+
+let films = [
+  { titel: "The Outpost", prijs: 10, vanaf: 16, gerne: "oorlog" },
+  {
+    titel: "De Piraten van Hiernaast",
+    prijs: 7,
+    vanaf: 6,
+    gerne: "kinderfilm",
+  },
+  { titel: "The Wretched", prijs: 10, vanaf: 16, gerne: "horror" },
+  {
+    titel: "The King of Staten Island",
+    prijs: 10,
+    vanaf: 12,
+    gerne: "komedie",
+  },
+];
+
+let aantalStoelen = 50;
+let geselecteerdeStoelen = 0;
+
+const filmsToList = () => {
+  films.forEach((film) => {
+    (newOption = document.createElement("option")),
+      (newOption.innerHTML = `${film.titel}  €${film.prijs}`),
+      (newOption.value = `${film.prijs}`);
+    filmOpties.appendChild(newOption);
+  });
+};
+
+filmsToList();
+
+const maakStoelen = (aantal) => {
+  for (let i = 0; i < aantal; i++) {
+    const newLi = document.createElement("li");
+    newLi.className = "vrij";
+    newLi.id = "stoelnr" + [i];
+    newLi.addEventListener("click", (event) => {
+      event.target.className == "selecteerd"
+        ? geselecteerdeStoelen--
+        : geselecteerdeStoelen++;
+      const toggleStoel = newLi.className == "vrij" ? "selecteerd" : "vrij";
+      newLi.className = toggleStoel;
+      onderTitel(geselecteerdeStoelen);
+    });
+    stoelenLijst.appendChild(newLi);
+  }
+};
+
+const stoelSelecteren = (event) => {
+  if (event.target.className == "selecteerd") {
+    geselecteerdeStoelen++;
+  }
+};
+
+maakStoelen(aantalStoelen);
+
+const onderTitel = (aantal) => {
+  const zin = document.getElementById("eindzin");
+  const filmprijs = document.getElementById("films").value;
+  const totaal = aantal * filmprijs;
+  zin.innerHTML = `Je hebt ${aantal} stoel(en) geselecteerd, per stuk kost €${filmprijs}. Dat is totaal €${totaal}`;
+};
